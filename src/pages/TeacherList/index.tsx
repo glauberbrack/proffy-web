d354a98858d73cbf57bf00ca46bf6d882f2b6c80
Glauber Brack <glauber@brack.com.br>
2020-08-10 00:08:45 -0300

    finishing styles for teacher list

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 const TeacherList = () => {
@@ -25,6 +27,35 @@ const TeacherList = () => {
                     </div>
                 </form>
             </Header>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars3.githubusercontent.com/u/57924169?s=460&u=18511518353cce8f19adf37e6b8098e71e0b5faf&v=4" alt="Glauber Brack"/>
+                    
+                        <div>
+                            <strong>Glauber Brack</strong>
+                            <span>Chemistry</span>
+                        </div>
+                    </header>
+
+                    <p>
+                    Mussum Ipsum, cacilds vidis litro abertis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Interagi no mé, cursus quis, vehicula ac nisi. Delegadis gente finis, bibendum egestas augue arcu ut est. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Price per Hour
+                            <strong>$ 100,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="WhatsApp"/>
+                            Get in touch
+                        </button>
+                    </footer>
+                    
+                </article>
+            </main>
         </div>
     )
 }
