6c43c7834dc90a6efce83b76c1aa305b7450719f
Glauber Brack <glauber@brack.com.br>
2020-08-11 14:51:43 -0300

    using api to add a new connection to api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -19,6 +20,11 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
         <article className="teacher-item">
             <header>
@@ -39,7 +45,7 @@ const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
                     Price per Hour
                     <strong>$ {teacher.price}</strong>
                 </p>
-                <a href={`https://wa.me/${teacher.whatsapp}`} target="blank">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="blank">
                     <img src={whatsappIcon} alt="WhatsApp"/>
                     Get in touch
                 </a>
