9f0630907a653c966ceb6fc1a50fbd0c612f9bc2
Glauber Brack <glauber@brack.com.br>
2020-08-11 11:12:38 -0300

    using select component and teacherlsit and editing css

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../../components/Header';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
@@ -11,8 +12,33 @@ const TeacherList = () => {
         <div id="page-teacher-list" className="container">
             <Header title="Look at those amazing Proffys available!">
                 <form id="search-teachers">
-                    <Input name="subject" label="Subject" />
-                    <Input name="week_day" label="Week Day" />
+                <Select
+                    name="subject"
+                    label="Subject"
+                    options={[
+                        {value: 'Arts', label: 'Arts'},
+                        {value: 'Biology', label: 'Biology'},
+                        {value: 'Sciences', label: 'Sciences'},
+                        {value: 'Physical Education', label: 'Physical Education'},
+                        {value: 'Physics', label: 'Physics'},
+                        {value: 'Chemistry', label: 'Chemistry'},
+                        {value: 'Portuguese', label: 'Portuguese'},
+                    ]}
+                />
+
+                <Select
+                    name="week_day"
+                    label="Week Day"
+                    options={[
+                        {value: '0', label: 'Sunday'},
+                        {value: '1', label: 'Monday'},
+                        {value: '2', label: 'Tuesday'},
+                        {value: '3', label: 'Wednesday'},
+                        {value: '4', label: 'Thursday'},
+                        {value: '5', label: 'Friday'},
+                        {value: '6', label: 'Saturday'},
+                    ]}
+                />
                     <Input type="time" name="time" label="Hour" />
                 </form>
             </Header>
